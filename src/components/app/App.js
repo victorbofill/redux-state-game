506fbc9f56e63a41f09aa2c4341f9a9c3fcbee02
victorbofill <victorm.bofill@gmail.com>
2018-06-12 09:14:53 -0700

    minimal app up and running

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>App running!</h1>
+      </main>
+    );
+  }
+}
---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+// import { REDUCERS } from REDUCERS;
+
+// const rootReducer = combineReducers({
+// })
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  // rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk
+    )
+  )
+);
+
+export default store;
