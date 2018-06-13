f60c4c0508fbf167ee6beb3b84dae4dbae1bda6d
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-13 09:46:50 -0700

    changed card flip to different react structure, not working still

--- src/components/game/Card.js
+++ src/components/game/Card.js
@@ -1,42 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.css';
-//import ReactCardFlip from 'react-card-flip';
+import CardFlippable from 'react-card-flippable';
+//import logo from './img/alchemy.png';
 
 export default class Card extends Component {
   static propTypes = {
     image: PropTypes.any
   };
 
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     isFlipped: false
-  //   };
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-   
-  // handleClick(e) {
-  //   e.preventDefault();
-  //   this.setState({ isFlipped: !this.state.isFlipped });
-  // }
   
   render() {
+    //const frontContent = { image };
+    //const backContent = { image };
     const { image } = this.props;
     return (
-      <td className={styles.card}>
-        <img src={image}onClick={this.handleClick}> 
-          {/* <ReactCardFlip isFlipped={this.state.isFlipped}>
-            <YOUR_FRONT_COMPONENT key="front">
-                This is the front of the card.
-              </YOUR_FRONT_COMPONENT>
-              <YOUR_BACK_COMPONENT key="back">
-                This is the back of the card.
-                <onClick={this.handleClick}>
-              </YOUR_BACK_COMPONENT>
-          </ReactCardFlip> */}
-        </img>
-      </td>
+      <div>
+        <td className={styles.card}>
+          <img src={image}onClick={this.handleClick}> 
+            {/* <CardFlippable frontContent={frontContent} backContent={backContent}/> */}
+          </img>
+        </td>
+      </div>
     );
   }
 }
